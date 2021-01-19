5bac3fde5c5a80ce0011d76d0020ce1d8b164f42
Patchrik <pklly92@gmail.com>
2021-01-19 11:19:29 -0600

    Added services and looks like we have post comments available to us now

--- Tabloid-Fullstack/Repositories/ICommentRepository.cs
+++ Tabloid-Fullstack/Repositories/ICommentRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace Tabloid_Fullstack.Repositories
+﻿using System.Collections.Generic;
+using Tabloid_Fullstack.Models;
+
+namespace Tabloid_Fullstack.Repositories
 {
     public interface ICommentRepository
     {
+        public List<Comment> GetByPostId(int id);
+
+        public void Add(Comment category);
     }
 }
