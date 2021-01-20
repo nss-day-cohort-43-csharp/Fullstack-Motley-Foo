54f7a70eff1525a79bd7015f6a7fe715940dbfe2
Patchrik <pklly92@gmail.com>
2021-01-20 14:09:43 -0600

    The forms seem to be working at catching the right data, will handle the api next

---
+++ Tabloid-Fullstack/Controllers/CommentController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Tabloid_Fullstack.Models;
+using Tabloid_Fullstack.Models.ViewModels;
+using Tabloid_Fullstack.Repositories;
+
+namespace Tabloid_Fullstack.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentController : ControllerBase
+    {
+
+        private ICommentRepository _commentRepo;
+        private IUserProfileRepository _userRepo;
+
+        public CommentController(ICommentRepository commentRepo, IUserProfileRepository userRepo)
+        {
+            _commentRepo = commentRepo;
+            _userRepo = userRepo;
+        }
+
+
+        [HttpPost]
+        public IActionResult Post(Comment comment)
+        {
+            _commentRepo.Add(comment);
+            return CreatedAtAction("Get", new { id = comment.Id }, comment);
+        }
+    }
+}
--- Tabloid-Fullstack/client/src/components/PostComments.js
+++ Tabloid-Fullstack/client/src/components/PostComments.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import './PostComments.css';
 import formatDate from '../utils/dateFormatter';
+import { useParams } from 'react-router-dom';
+import { UserProfileContext } from '../providers/UserProfileProvider';
 
 const PostComments = ({ postComments }) => {
+  const { getToken, getCurrentUser } = useContext(UserProfileContext);
+  const { postId } = useParams();
+  const [commentSubject, setCommentSubject] = useState('');
+  const [commentContent, setCommentContent] = useState('');
+
+  useEffect(() => {
+    console.log('This is the commentSubject state', commentSubject);
+    console.log('This is the commentContent state', commentContent);
+  }, [commentSubject, commentContent]);
+
+  const saveNewComment = () => {
+    const currentUser = getCurrentUser();
+    const commentToAdd = {
+      postId: postId,
+      userProfileId: currentUser.id,
+      subject: commentSubject,
+      content: commentContent,
+    };
+    console.log(commentToAdd);
+  };
+
   return (
     <div className="container mt-5">
       <div className="d-flex justify-content-center row">
@@ -35,18 +58,36 @@ const PostComments = ({ postComments }) => {
           <div className="d-flex flex-column comment-section">
             <div className="p-2">
               <div className="d-flex flex-row align-items-start">
-                <textarea className="form-control ml-1 shadow-none textarea"></textarea>
+                <textarea
+                  value={commentSubject}
+                  placeholder="Comment Subject"
+                  className="form-control ml-2 shadow-none border textarea"
+                  onChange={(e) => setCommentSubject(e.target.value)}
+                ></textarea>
+              </div>
+              <div className="d-flex flex-row align-items-start">
+                <textarea
+                  value={commentContent}
+                  placeholder="Comment Content"
+                  className="form-control ml-2 shadow-none border textarea"
+                  onChange={(e) => setCommentContent(e.target.value)}
+                ></textarea>
               </div>
               <div className="mt-2 text-right">
                 <button
                   className="btn btn-primary btn-sm shadow-none"
                   type="button"
+                  onClick={saveNewComment}
                 >
                   Post comment
                 </button>
                 <button
                   className="btn btn-outline-primary btn-sm ml-1 shadow-none"
                   type="button"
+                  onClick={(click) => {
+                    setCommentContent('');
+                    setCommentSubject('');
+                  }}
                 >
                   Cancel
                 </button>
