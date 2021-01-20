b6c73aa836cccada680c2cdef8a57b3a09073705
Travis Pinson <travispinson@gmail.com>
2021-01-20 11:34:21 -0600

    Fixed errors showing from null post imagelocations

--- Tabloid-Fullstack/client/src/components/PostSummaryCard.js
+++ Tabloid-Fullstack/client/src/components/PostSummaryCard.js
@@ -5,17 +5,36 @@ import formatDate from "../utils/dateFormatter";
 import "./PostSummaryCard.css";
 
 const PostSummaryCard = ({ post }) => {
+
+  const ImageCard = () => {
+    if (post.imageLocation === null) {
+      return (
+        <div
+          className="post-summary__img"
+          style={{
+            backgroundImage: `url('https://build.dfomer.com/wp-content/uploads/2016/04/dummy-post-horisontal-thegem-blog-default.jpg')`,
+          }}
+        ></div>
+      )
+    }
+    else {
+      return (
+        <div
+          className="post-summary__img"
+          style={{
+            backgroundImage: `url(${post.imageLocation})`,
+          }}
+        ></div>
+      )
+    }
+  }
+
   return (
     <Card className="post-summary__card">
       <div className="row">
         <div className="col-lg-3 col-sm-12">
           <Link to={`/post/${post.id}`}>
-            <div
-              className="post-summary__img"
-              style={{
-                backgroundImage: `url(${post.imageLocation})`,
-              }}
-            ></div>
+            <ImageCard />
           </Link>
         </div>
         <div className="col-lg-5 col-sm-12 py-3">
