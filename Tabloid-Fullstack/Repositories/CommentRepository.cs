c158ba8aa122ce5c554e9299e276120a983d8ac0
Patchrik <pklly92@gmail.com>
2021-01-21 16:10:15 -0600

    Refactored the delete in the Comment repo and added Delete to the comment controller.

--- Tabloid-Fullstack/Repositories/CommentRepository.cs
+++ Tabloid-Fullstack/Repositories/CommentRepository.cs
@@ -25,14 +25,22 @@ namespace Tabloid_Fullstack.Repositories
                 .ToList();
         }
 
+        public Comment GetByCommentId(int id)
+        {
+            return _context.Comment.Where(c => c.Id == id).FirstOrDefault();
+        }
+
         public void Add(Comment comment)
         {
             _context.Add(comment);
             _context.SaveChanges();
         }
 
-        public void Delete(Comment comment)
+        public void Delete(int id)
         {
+
+            var comment = _context.Comment.Where(c => c.Id == id).FirstOrDefault();
+
             _context.Comment.Remove(comment);
             _context.SaveChanges();
         }
