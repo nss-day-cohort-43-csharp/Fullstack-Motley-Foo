2bbdb161d141af3a7ed71385f992748cfb6484b6
Travis Pinson <travispinson@gmail.com>
2021-01-26 13:14:49 -0600

    secured postreactions

--- Tabloid-Fullstack/Controllers/PostReactionController.cs
+++ Tabloid-Fullstack/Controllers/PostReactionController.cs
@@ -5,26 +5,44 @@ using System.Linq;
 using System.Threading.Tasks;
 using Tabloid_Fullstack.Repositories;
 using Tabloid_Fullstack.Models;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
 
 namespace Tabloid_Fullstack.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
 
     public class PostReactionController : ControllerBase
     {
         private IPostReactionRepository _postReactionRepo;
+        private IUserProfileRepository _userRepo;
 
-        public PostReactionController(IPostReactionRepository postReactionRepo)
+        public PostReactionController(IPostReactionRepository postReactionRepo, IUserProfileRepository userRepo)
         {
             _postReactionRepo = postReactionRepo;
+            _userRepo = userRepo;
         }
 
         [HttpPost]
         public IActionResult Post(PostReaction postReaction)
         {
+            var currentUser = GetCurrentUserProfile();
+
+            if (currentUser.Id != postReaction.UserProfileId)
+            {
+                return Unauthorized();
+            }
+
             _postReactionRepo.Add(postReaction);
             return NoContent();
+        }
+
+        private UserProfile GetCurrentUserProfile()
+        {
+            var firebaseUserId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            return _userRepo.GetByFirebaseUserId(firebaseUserId);
         }
     }
 }
--- Tabloid-Fullstack/client/src/components/PostReactions.js
+++ Tabloid-Fullstack/client/src/components/PostReactions.js
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState, useContext } from "react";
 import { Badge } from 'reactstrap';
 import './PostReaction.css';
+import { UserProfileContext } from "../providers/UserProfileProvider";
+import { useParams, useHistory } from 'react-router-dom';
 
-const addReaction = (event) => {
-  const reactionToAdd = event.target.id
-  console.log(reactionToAdd)
-}
+
 
 const PostReactions = ({ postReactions }) => {
+  const { getToken } = useContext(UserProfileContext);
+  const { postId } = useParams();
+  const history = useHistory();
+  const user = JSON.parse(localStorage.getItem('userProfile'));
+
+
+  const createReaction = (event) => {
+    const reactionToAdd = event.target.id
+    const newReaction = {
+      reactionId: parseInt(reactionToAdd),
+      userProfileId: user.id,
+      postId: parseInt(postId)
+    }
+    addReaction(newReaction)
+
+  }
+
+  const addReaction = (newReaction) => {
+    getToken().then((token) => {
+      fetch(`/api/postReaction`, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newReaction)
+      })
+      //.then(history.go(`/post/${postId}`))
+    })
+  }
+
   return (
     <div className="center">
       {postReactions.map((postReaction) => (
@@ -17,7 +47,7 @@ const PostReactions = ({ postReactions }) => {
             className="p-2 border border-dark post-reaction__pill"
             title={postReaction.reaction.name}
           >
-            <div id={postReaction.reaction.id} onClick={(event) => (addReaction(event))}>{postReaction.reaction.emoji} {postReaction.count}</div>
+            <div id={postReaction.reaction.id} onClick={(event) => (createReaction(event), postReaction.count++)}>{postReaction.reaction.emoji} {postReaction.count}</div>
           </Badge>
         </div>
       ))}
